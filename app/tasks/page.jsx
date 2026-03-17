6179dcee04876cb1b04a013d669336fc4f961fe7
Виктор <igro.mir.96@mail.ru>
2026-03-17 21:11:42 +0200

    добавил кэширование в лабораторную 5

--- app/tasks/page.jsx
+++ app/tasks/page.jsx
@@ -1,24 +1,40 @@
 import TaskList from "../components/TasksList";
+import { cacheLife } from "next/cache";
 
-export default async function TaskPage() {
+let requestCount = 0;
+
+//функция кэша
+async function getTasks() {
+  'use cache'
+  cacheLife('minutes'); 
+  
+requestCount++;
+console.log(`Запрос к API № ${requestCount}`)
+  
   const res = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10");
   const tasks = await res.json();
+  return tasks;
+}
+
+export default async function TaskPage() {
+  const tasks = await getTasks(); 
 
  return (
-    <div style={{ 
-      maxWidth: "1200px", 
-      margin: "0 auto", 
-      padding: "20px",
-      fontFamily: "system-ui, -apple-system, sans-serif"
-    }}>
-      <h1 style={{ 
-        fontSize: "2rem", 
-        color: "#333",
-        borderBottom: "3px solid #4f46e5",
-        paddingBottom: "10px"
+    <div>
+      <h1>Список задач</h1>
+      <div style={{
+        padding: "10px",
+        backgroundColor: "#ffedd5",
+        borderLeft: "4px solid #f97316",
+        marginBottom: "20px"
       }}>
-         Список задач
-      </h1>
+        <p style={{ margin: 0 }}>
+           <strong>Запросов к API:</strong> {requestCount}
+        </p>
+        <p style={{ margin: "5px 0 0 0", fontSize: "0.9rem" }}>
+          {requestCount === 1 ? " Первый запрос (данные в кэш)" : " Кэш обновлен"}
+        </p>
+      </div>
       <TaskList tasks={tasks} />
     </div>
   );
